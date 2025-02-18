de60607b037c2f12033094c79f7d803d48294ad8
pray63764 <pray63764@gmail.com>
2025-02-19 00:14:45 +0530

    Initial Commit

---
+++ IM_CONSOLE_PROJECT/Globals.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace IM_CONSOLE_PROJECT
+{
+    public static class Globals
+    {
+        public static string ENVIRONMENT_CODE = "PRODUCTION";
+    }
+}
---
+++ IM_CONSOLE_PROJECT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IM_CONSOLE_PROJECT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Vivek! Now in "+ Globals.ENVIRONMENT_CODE);
+            if (Globals.ENVIRONMENT_CODE == "PRODUCTION")
+            {
+                Console.WriteLine("Running in Production");
+            }
+            else if (Globals.ENVIRONMENT_CODE == "UAT")
+            {
+                Console.WriteLine("Running in UAT");
+            }
+        }
+    }
+}
